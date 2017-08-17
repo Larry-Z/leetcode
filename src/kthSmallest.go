3cc9b1b14377555090ba780f8b196ee1263b5a9e
zhengshiqiang <zhengsj03@163.com>
2017-08-18 00:27:57 +0800

    kth smallest in tree

---
+++ src/kthSmallest.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func kthSmallest(root *TreeNode, k int) int {
+	if root == nil {
+		return -1
+	}
+	i := 1
+	stack := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+        if i == k {
+            return root.Val
+        }
+        i++
+        root = root.Right
+	}
+    return -1
+}
+
+func main() {
+    list := "20,16,23,14,18,21,25,null,15,17,null";
+    root := listToTree(list)
+    printTreePretty(root, 0)
+    fmt.Println(kthSmallest(root, 4))
+}
